9dc7116da68b0933a83bf6ae8e8162634883a0a0
Radee Al Mahmood <radee.mahmood@northsouth.edu>
2022-02-06 19:04:58 +0600

    cachematrix function done

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinvrse <- function(invrse) i <<- invrse
+  getinvrse <- function() invrse
+  list(set = set, get = get,
+       setinvrse = setinvrse,
+       getinvrse = getinvrse)
 }
 
 
